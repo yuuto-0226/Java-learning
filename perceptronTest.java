756e6655bfa77ce84f3d38c89805ce4b27f39e80
yuuto-0226 <69916482+yuuto-0226@users.noreply.github.com>
2020-08-20 16:03:15 +0800

    Update perceptronTest.java

--- perceptronTest.java
+++ perceptronTest.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 public class perceptronTest {
-	private static double e;//閥值
-	private static double w;//權術
+	private static float e;//閥值
+	private static float w;//權術
 	private static float[] rate;//正確率(%)
 	private static int count;//次數
-	private static double Eta=0.005;//學習速率
-	private int x;//輸入值
-	
-	public perceptronTest(float rate,int count) {
+	private static float Eta=0.5f;//學習速率
+	public perceptronTest(float rate,int count) { //建構元
 		this.e=0;
 		this.w=0;
-		this.x=0;
 		this.count=count;
 		this.rate=new float[count];
 	}
@@ -19,12 +16,13 @@ public class perceptronTest {
 		for(int i=0;i<count;i++) {
 			int erroCount=0;
 			for(int[] sample:samples) {
-				double update=Eta*(sample[1]-sample[0]);
-				
+				double update=bias(sample[0]);//Eta*(sample[1]-fuc(sample[0]));
 				w+=update*sample[0];
 				e+=update;
+				if(update!=0)
+					erroCount++;
 			}
-			rate[i]=1-erroCount*1.0f/samples.length;
+			rate[i]=1-erroCount*1.000000f/samples.length;
 		}
 	}
 	//sigMoid函數
@@ -40,38 +38,38 @@ public class perceptronTest {
 	}
 	//激發函數，輸出值即為Y
 	public static double fuc(int x) {
-		return net(x)>=0?0:1;
+		return (net(x)>=0)?1:0;
+		//return net(x)>=0?0:1;
+		//return sigMoid(net(x));
 	}
 	//修正權值
-	public double bias(int x,double net) {
+	public static double bias(int x) {
 		double update;
-		return update=Eta*(fuc(x)-(double)x);
+		return update=Eta*(fuc(x)-((x>=0)?1:0));
 	}
 	//印出修正
 	public static void printRate() {
 		for(int i=0;i<count;i++)
-			System.out.print("[#"+i+"]"+rate[i]+"\n");
-	}
-		
+			System.out.print("[#"+i+"]"+rate[i]*1.00000f+"\n");
+	}	
 	//學習樣本
 	public static int[][] getStudyData(){
-		int[][] data=new int[2000000][2];
-		for(int i=-100000,j=100000;i<100000;i++,j++) {
+		int[][] data=new int[2000][2];
+		for(int i=-100,j=100;i<100;i++,j++) {
 			data[j][0]=i;
-			data[j][1]=(i>=0)?1:0;//-10000~10000間>0輸出1反之輸出0
+			data[j][1]=(i>0)?1:0;//-100000~100000間>0輸出1反之輸出0
 		}
 		return data;
 	}
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		perceptronTest perceptron=new perceptronTest(0.0000005f,100);
+		perceptronTest perceptron=new perceptronTest(0.0000005f,1000);
 		perceptronTest.fit(getStudyData());
 		perceptronTest.printRate();
 		while(true) {
-		System.out.print("[#]  ");
-		int x=sc.nextInt();
-		
-		System.out.println("[#]"+perceptronTest.fuc(x));
+			System.out.print("[#]  ");
+			int x=sc.nextInt();
+			System.out.println("[#]"+perceptronTest.fuc(x));
 		}
 	}
 }
