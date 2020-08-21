2e88c7e0e41c0c8c554d5f8ef566e0cc60b6a731
yuuto-0226 <69916482+yuuto-0226@users.noreply.github.com>
2020-08-21 14:50:01 +0800

    Update perceptronTest.java

--- perceptronTest.java
+++ perceptronTest.java
@@ -1,22 +1,24 @@
-package AI00;
-import java.util.Scanner;
+import java.util.*;
 public class perceptronTest {
 	private static float T;//預估值
 	private static float Y;//激發函數輸出值
-	private float x;//輸入值
+	private static float x;//輸入值
 	private static float w;//權值
 	private static float theta;//閥值
 	private static float eta;//學習速率
 	private static float bias;//修正值
-	private static float rate;//正確率
+	private static int[] rate;//正確率
 	private static int count;//錯誤計數
+	private static Scanner sc;//輸入
+	
 	//設定初始值
 	public perceptronTest() {
-		this.theta=0;
-		this.w=0;
-		this.bias=0;
-		this.rate=0;
-		this.count=0;
+		theta=0;
+		w=0;
+		bias=0;
+		rate=new int[1000000000];
+		count=0;
+		eta=0.5f;
 	}
 	
 	//求和函數(ΣXi*Wi-θ)
@@ -35,17 +37,17 @@ public class perceptronTest {
 	public static void fit(int Data) {
 		while(bias!=0) {
 			Y=actFunc(sigma(Data));	//激發函數輸出值
-			T=(Data>0)?Data:0;	//預估值
+			T=(Data>0)?1:0;	//預估值
 			bias=eta*(T-Y)*Data;
 			
 			w+=bias;
 			theta+=bias/Data;
 			
-			rate=Y/T*1.00f;
+			rate[count]=(int)(Y/T*100);
 			
 			count++;
 			
-			System.out.println("[AI]#"+count+" >> "+rate);
+			System.out.println("[AI]#"+count+" >> "+rate[count]);
 		}
 	}
 	
@@ -54,9 +56,17 @@ public class perceptronTest {
 		int data=(min-1)+(int)(Math.random()*(max-min)+1);
 		return data;
 	}
+	//印出
+	public void printPer() {
+
+	}
 	
 	
 	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		System.out.print("[#] ");
+		x=sc.nextInt();
 		fit(Data(100,-100));
+		actFunc(sigma((int)x));
 	}
 }
